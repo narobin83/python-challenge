2df484de664f63d2f5752577f32b614dc2a300c7
Norman Robinson <normanrobinson@Normans-MBP.attlocal.net>
2021-03-20 00:36:30 -0400

    pybank_csv.py

--- PyBank/pybank_csv.py
+++ PyBank/pybank_csv.py
@@ -4,30 +4,30 @@ import csv
 csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
     
 
-with open(csvpath, 'r') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
+with open('budget_data.csv', 'r') as csvfile:
+    csv_reader = csv.Dictreader(csvfile, delimiter=',') 
+    total = []
+    for row_count, row in enumerate(csvreader, start=1)
+        value = int(row['Profit/Losses'])
+        totals.append(value)    
 
-   
-output_path = os.path.join("..", "PyBank", "analysis.csv")
+print("Financial Analysis")
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+print("Total Months: {}".format(row_count))
+print("Total: {}".format(sum(totals)))
+print("Average Change: ")
+print("Greatest Increase in Profits: ")
+print("Greatest Decrease in Profits: ")
+        
 
-with open(output_path, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',')
+output_path = os.path.join("Analysis.txt")    
 
-    csvwriter.writerow(['text'])
-    
-    csvwriter.writerow(['Financial_Analysis'])
+with open(output_file, 'w', newline='') as text_file:
+    text_file.write("Financial Analysis\n")
+    text_file.write("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n")
+    text_file.write("Total Months: {}\n".format(row_count))
+    text_file.write("Average: {}\n")
+    text_file.write("Greatest Increase in Profits: {}\n")
+    text_file.write("Greastest Decrease in Profits: {}\n")
 
-print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
-
-with open(output_path, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    csvwriter.writerow(['Total Months:'])
-
-    csvwriter.writerow(['Total:'])
-
-    csvwriter.writerow(['Average Change:'])
-
-    csvwriter.writerow(['Greates Increase in Profits:'])
-
-     csvwriter.writerow(['Greatest Decrease in Profits:'])
+  
