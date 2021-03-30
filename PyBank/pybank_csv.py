8d527cb4ab6b295c87dc3ae1bbbb09123e998bde
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-03-30 19:13:04 -0400

    pybank_csv.py

--- PyBank/pybank_csv.py
+++ PyBank/pybank_csv.py
@@ -2,78 +2,6 @@ import os
 import csv
 
 csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
-
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
-
-def Analysis(data):
-
-    # Variables
-    Months_Amount = 0
-    Profit = 0
-    Months = []
-    Total_Profit = 0
-    Difference = 0
-    Total_Difference = []
-    
-    # Loop
-    for row in data:
-       
-        # + 1
-        Months_Amount += 1
-        
-        # Total_Profit total
-        Total_Profit += int(row[1])
-        
-        # Months total
-        Months.append(str(row[0]))
-        
-        if Difference != 0:
-            
-            # First Profit 
-            Profit = int(row[1])
-            
-            # Difderence, new vs old
-            Difference = Profit - Difference
-            
-            # Store Difference 
-            Total_Difference.append(Difference)
-            
-            # Reset
-            Difference = int(row[1])
-            
-        # Else value = 0
-        elif Difference == 0:
-            Difference = int(row[1])  
-            
-    # No Difference first month
-    Months.pop(0)
-    
-    # Find Greatest Increase and
-    Greatest_Increase = Total_Difference.index(max(Total_Difference))
-    Greatest_Decrease = Total_Difference.index(min(Total_Difference))
-
-    # Find Months
-    Increase_Difference = (Months[int(Greatest_Increase)], max(Total_Difference))
-    Decrease_Difference = (Months[int(Greatest_Decrease)], min(Total_Difference))
-    
-# Find mean of total difference
-    Mean = sum(Total_Difference)/float(len(Total_Difference))
-    Mean = round(Mean,2)
-    
-    # print the results
-    print(f'Financial Analysis')
-    print(f'-------------------------------------------')
-    print(f'Total Months: {Months_Amount}')
-    print(f'Net Profit: {Total_Profit}')
-    print(f'Average Monthly Change: {Mean}')
-    print(f'Greatest Increase in Profits: {Increase_Difference}')
-    print(f'Greatest Loss In Profits: {Decrease_Difference}')
-
-
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -86,22 +14,31 @@ with open(csvpath) as csvfile:
     
     print(f"CSV Header: {csv_header}")
 
-    # Specify the file to write to
-output_path = os.path.join("..", "PyBank", "analysis.csv")
+  #create empty lists to add the csv values to 
+    month_count = []
+    profit = []
+    change_profit = []
 
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as csvfile:
+ #iterate through the values and add them to the empty list 
+    for row in csvreader:
+        month_count.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
+        change_profit.append(profit[i+1]-profit[i])
+                      
+#evaluate the max and min from the list made
+increase = max(change_profit)
+decrease = min(change_profit)
 
-    # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
+#using the index, 
+month_increase = change_profit.index(max(change_profit))+1
+month_decrease = change_profit.index(min(change_profit))+1
 
-    # Write the first row (column headers)
-    csvwriter.writerow(['Financial Analysis'])
-
-    # Write the second row
-    csvwriter.writerow(['Total Month: 86'])
-    csvwriter.writerow(['Net Profit: $38382578'])
-    csvwriter.writerow(['Average Monthly Change: $-2315.12'])
-    csvwriter.writerow(['Greatest Increase In Profits: Feb-2012 ($1926159'])
-    csvwriter.writerow(['Greastest Loss In Profits: Sep-2013 ($-2196167'])
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months:{len(month_count)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+print(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")
 
